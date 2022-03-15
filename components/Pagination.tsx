3b1535d5be38e1668e647d50894cdcccd6f7238b
Kekelberi <alexei.gaponov@gmail.com>
2022-03-15 16:15:21 +0300

    save

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,17 +1,21 @@
 import { NextPage } from 'next'
 import React from 'react'
 
-const Pagination: NextPage = ({ postsPerPage, totalPosts, paginate }) => {
+const Pagination: NextPage<{
+  postsPerPage: number
+  totalPosts: number
+  paginate: (number: number) => number
+}> = ({ postsPerPage, totalPosts, paginate }) => {
   const pageNumbers = []
 
-  for (let i = 1; i <= (totalPosts / postsPerPage); i++) {
+  for (let i = 1; i <= totalPosts / postsPerPage; i++) {
     pageNumbers.push(i)
   }
 
   return (
     <nav>
       <ul>
-        {pageNumbers?.map((number: any) => (
+        {pageNumbers?.map((number: number) => (
           <a href='#!' key={number} onClick={() => paginate(number)}>
             {number}
           </a>
